8540c984d1f129637a0e3c54a0e8d786f96f1457
fersoliz77 <alberdimoreno0@gmail.com>
2026-03-14 12:54:48 -0300

    lp 2

--- web/check-plans.ts
+++
@@ -1,9 +0,0 @@
-import { createSupabaseAdminClient } from "./src/infrastructure/supabase/client/admin.ts";
-
-async function checkPlans() {
-  const supabase = createSupabaseAdminClient();
-  const { data, error } = await supabase.from('plans').select('*');
-  console.log(JSON.stringify(data, null, 2));
-}
-
-checkPlans();
